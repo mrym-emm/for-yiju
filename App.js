03ce6a19d0a65f6bc18fcf3662844d92ae9b60f7
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-04-16 15:47:19 +0800

    Just focus on the YijuPhisihing and App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import YijuPhishingSimulator from './YijuPhishingSimulator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Components</h1>
+      </header>
+      <main>
+        <YijuPhishingSimulator />
+      </main>
+    </div>
+  );
+}
+
+export default App;
