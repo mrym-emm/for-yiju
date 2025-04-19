855fb593f9a9fd149623b1bed19f325a340347a7
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-04-19 19:22:46 +0800

    update files

--- YijuStoryGame.js
+++ YijuStoryGame.js
@@ -4,7 +4,7 @@ import React from 'react';
 const YijuStorygame = () => {
   return (
     <div className="game-container">
-      <iframe frameborder="0" src="https://itch.io/embed-upload/13399556?color=333333" allowfullscreen="" width="640" height="380"><a href="https://m4meh.itch.io/caknak-cyberquest">Play CAKNAK: Cyber Quest on itch.io</a></iframe>
+      <iframe frameborder="0" src="https://itch.io/embed-upload/13425410?color=333333" allowfullscreen="" width="640" height="380"><a href="https://m4meh.itch.io/caknak-cyberquest">Play CAKNAK: Cyber Quest on itch.io</a></iframe>
     </div>
   );
 };
